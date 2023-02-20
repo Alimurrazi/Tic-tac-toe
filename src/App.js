50a427f8bcef8612bc9d47d4dbfffd0cb3283510
Alimur Razi Rana <alimurrazi570@gmail.com>
2023-02-20 11:46:56 +0600

    initialized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Cell from "./Cell";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+
+const checkIfGameOver = (board) => {
+  let result = {
+    status: false,
+    winningCells: []
+  }
+  // Check if any row has the same value
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i].columns[0].value !== "" &&
+      board[i].columns[0].value === board[i].columns[1].value &&
+      board[i].columns[1].value === board[i][2].value
+    ) {
+      result = {
+        status: true,
+        winningCells: [board[i].columns[0].id, board[i].columns[1].id, board[i].columns[2].id]
+      }
+    }
+  }
+
+  // Check if any column has the same value
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[0].columns[i].value !== "" &&
+      board[0].columns[i].value === board[1].columns[i].value &&
+      board[1].columns[i].value === board[2].columns[i].value
+    ) {
+      result = {
+        status: true,
+        winningCells: [board[0].columns[i].id, board[1].columns[i].id, board[2].columns[i].id]
+      }
+    }
+  }
+
+  // Check the first diagonal
+  if (
+    board[0].columns[0].value !== "" &&
+    board[0].columns[0].value === board[1].columns[1].value &&
+    board[1].columns[1].value === board[2].columns[2].value
+  ) {
+    result = {
+      status: true,
+      winningCells: [board[0].columns[0].id, board[1].columns[1].id, board[2].columns[2].id]
+    }
+  }
+
+  // Check the second diagonal
+  if (
+    board[0].columns[2].value !== "" &&
+    board[0].columns[2].value === board[1].columns[1].value &&
+    board[1].columns[1].value === board[2].columns[0].value
+  ) {
+    result = {
+      status: true,
+      winningCells: [board[0].columns[2].id, board[1].columns[1].id, board[2].columns[0].id]
+    }
+  }
+
+  return result;
+};
 
 function App() {
   let cellData = [
@@ -31,22 +90,51 @@ function App() {
     },
   ];
 
-  const [cellState, setCellState] = useState(cellData);
-  const [isMachinePlayer, setIsMachinePlayer] = useState(false);
+  const [cellStates, setCellStates] = useState(cellData);
+  const [isFirstPlayer, setIsFirstPlayer] = useState(true);
+  const [winStatus, setWinStatus] = useState(false);
+  const [winningCells, setWinningCells] = useState([]);
+  useEffect(()=>{
+    console.log(cellStates);
+    const result = checkIfGameOver(cellStates);
+    if(result.status) {
+      setWinStatus(true);
+      setWinningCells(result.winningCells);
+    } else {
+      if(isFirstPlayer) {
+        setIsFirstPlayer(false);
+      } else {
+        setIsFirstPlayer(true);
+      }
+    }
+  },[cellStates])
+
+  useEffect(()=>{
+    if(winStatus) {
+      console.log(isFirstPlayer);
+    }
+  },[winStatus, isFirstPlayer]);
 
   return (
     <div className="App">
       <div className="App-content">
-        
+
         <div className="mb-20">
-          {isMachinePlayer ? (<div>Machine Turns Now</div>):(<div>Your Turns Now</div>)}
+        {!winStatus && isFirstPlayer && <div>First Player's Turn Now</div>}
+         {!winStatus && !isFirstPlayer && <div>Second Player's Turn Now</div>}
+         {winStatus && isFirstPlayer && <div>First Player Wins</div>}
+         {winStatus && !isFirstPlayer && <div>Second Player Wins</div>}
         </div>
 
         <div className="board">
           {cellData.map((row) => (
             <div className="row" key={row.rowId}>
               {row.columns.map((col) => (
-                <Cell key={col.id} columnId={col.id} cellState={cellState} setCellState={setCellState}></Cell>
+                <Cell key={col.id} columnId={col.id}
+                cellClass={winStatus && winningCells.includes(col.id) ? 'cell winning-cell': 'cell'}
+                cellStates={cellStates} setCellStates={setCellStates} isFirstPlayer={isFirstPlayer}
+                winStatus={winStatus}>
+                </Cell>
               ))}
             </div>
           ))}
--- src/Cell.js
+++ src/Cell.js
@@ -1,78 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Cell.css";
 
-const checkIfGameOver = (board) => {
-  // Check if any row has the same value
-  for (let i = 0; i < 3; i++) {
-    if (
-      board[i].columns[0].value !== "" &&
-      board[i].columns[0].value === board[i].columns[1].value &&
-      board[i].columns[1].value === board[i][2].value
-    ) {
-      return true;
-    }
-  }
-
-  // Check if any column has the same value
-  for (let i = 0; i < 3; i++) {
-    if (
-      board[0].columns[i].value !== "" &&
-      board[0].columns[i].value === board[1].columns[i].value &&
-      board[1].columns[i].value === board[2].columns[i].value
-    ) {
-      return true;
-    }
-  }
-
-  // Check the first diagonal
-  if (
-    board[0].columns[0].value !== "" &&
-    board[0].columns[0].value === board[1].columns[1].value &&
-    board[1].columns[1].value === board[2].columns[2].value
-  ) {
-    return true;
-  }
-
-  // Check the second diagonal
-  if (
-    board[0].columns[2].value !== "" &&
-    board[0].columns[2].value === board[1].columns[1].value &&
-    board[1].columns[1].value === board[2].columns[0].value
-  ) {
-    return true;
-  }
-
-  return false;
-};
-
-function Cell({ columnId, cellState, setCellState }) {
+function Cell({ columnId, cellStates, setCellStates, isFirstPlayer, winStatus, cellClass }) {
   let isChecked = false;
   const [cellValue, setCellValue] = useState("");
+  const [rowNo, setRowNo] = useState(1);
+  const [indexNo, setIndexNo] = useState(0);
 
   function updateValue() {
-    if (!isChecked) {
+    if (!isChecked && !winStatus) {
       const rowNumber = Math.floor(columnId / 10);
-      const index = cellState[rowNumber - 1].columns.findIndex(
+      const index = cellStates[rowNumber - 1].columns.findIndex(
         (column) => column.id === columnId
       );
-      const value = cellState[rowNumber - 1].columns[index].value;
+      const value = cellStates[rowNumber - 1].columns[index].value;
       if (!value) {
-        setCellValue("X");
-        const updatedState = JSON.parse(JSON.stringify(cellState));
-        updatedState[rowNumber - 1].columns[index].value = "X";
-        setCellState(updatedState);
-
-        const status = checkIfGameOver(updatedState);
-        if(status) {
-            alert(status);
+        setRowNo(rowNumber);
+        setIndexNo(index);
+        if(isFirstPlayer) {
+          setCellValue("X");
+        } else {
+          setCellValue("0");
         }
-
       }
     }
   }
 
+  useEffect(()=>{
+    const updatedState = JSON.parse(JSON.stringify(cellStates));
+    updatedState[rowNo - 1].columns[indexNo].value = cellValue;
+    setCellStates(updatedState);
+  },[cellValue, indexNo, rowNo]);
+
   return (
-    <div className="cell" onClick={() => updateValue()}>
+    <div className={cellClass} onClick={() => updateValue()}>
       {cellValue}
     </div>
   );
